8f0b6b3759eb211aff1386f7f1fbe00dcf428056
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-01-25 22:02:57 -0500

    Synchronize play and stop on two users

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import {socket} from './socket'
+import React, {useEffect} from 'react'
 import Youtube from 'react-youtube'
 
 function Video(){
+    useEffect(() => {
+        socket.connect();
+    })
+
     function onStateChange(event){
         //access to the player object through event.target
         const state = event.target.getPlayerState();
+        //const timeStamp = event.target.getCurrentTime();
+        // bind the event handler to the socket in case of server notify the client to update player status
+        socket.on('state', (state)=> {
+            switch (state) {
+                case 1: // playing
+                    event.target.playVideo();
+                    break;
+                case 2: // paused
+                    event.target.pauseVideo();
+                    break;
+                default:
+                    break;
+            }
+        })
+
+        // sending player status to the server
         switch (state) {
-            case 2: // paused
-                // do something
-                console.log(state);
-                break;
             case 1: // playing
-                // do something
-                console.log(state);
+                socket.emit('state',1,);
+                break;
+            case 2: // paused
+                socket.emit('state',2,);
                 break;
             default:
                 break;
-        }    
+        }
     } 
-       <Youtube videoId="WIRK_pGdIdA"></Youtube>
+
     return (
         <div className='flex justify-center pt-8'>
             <Youtube videoId="WIRK_pGdIdA" onStateChange={onStateChange}></Youtube>
