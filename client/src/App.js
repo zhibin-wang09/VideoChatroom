04639420fc1c583cd1016998068c8826947bcd9c
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-01-26 10:51:01 -0500

    Synchronize play/pause and extract event.target object out from the youtube component for better control"

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,54 @@
 import Navbar from "./Navbar";
 import Video from "./Video";
 import {socket} from './socket';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(() => {
+    const [player, setPlayer] = useState(null); // store  event.target(which is the object we need to control the video)
+
+    useEffect(() => {
         // connect to the server only when this component is mounted
         socket.connect();
-
-        // clean up: disconnect the socket to prevent duplicate event registration
-        return () => {
-          socket.disconnect();
+    
+        let playerListener; // variable to store the event listener
+    
+        // Function to handle state changes received from the server
+        const handleStateChange = (state) => {
+            switch (state) {
+                case 1: // playing
+                    player.playVideo();
+                    break;
+                case 2: // paused
+                    player.pauseVideo();
+                    break;
+                default:
+                    break;
+            }
+        };
+    
+        if (player) {
+            // If the player is ready, register the event listener
+            playerListener = socket.on('state', handleStateChange);
         }
-    })
+    
+        // clean up: disconnect the socket and remove the player listener
+        return () => {
+            socket.disconnect();
+            if (playerListener) {
+                playerListener.off('state', handleStateChange);
+            }
+        };
+    }, [player]); // re-run the effect when the player changes
+
+    function onReady(event){ // set up the player object when the video is loaded
+        setPlayer(event.target);
+    }
 
     function onStateChange(event){
         //access to the player object through event.target
         const state = event.target.getPlayerState();
         //const timeStamp = event.target.getCurrentTime();
-        // bind the event handler to the socket in case of server notify the client to update player status
-        console.log(state);
-        socket.on('state', (state)=> {
-            switch (state) {
-                case 1: // playing
-                    event.target.playVideo();
-                    break;
-                case 2: // paused
-                    event.target.pauseVideo();
-                    break;
-                default:
-                    break;
-            }
-        })
-
+        
         // sending player status to the server
         switch (state) {
             case 1: // playing
@@ -45,13 +61,13 @@ function App() {
                 break;
         }
     } 
-  return (
-   <>
+    return (
+    <>
 
-    <Navbar></Navbar>
-    <Video onStateChange={onStateChange}></Video>
-   </> 
-  );
+        <Navbar></Navbar>
+        <Video onStateChange={onStateChange} onReady={onReady}></Video>
+    </> 
+    );
 }
 
 export default App;
--- client/src/Video.js
+++ client/src/Video.js
@@ -2,12 +2,12 @@
 import React  from 'react'
 import Youtube from 'react-youtube'
 
-function Video({onStateChange}){
+function Video({onStateChange,onReady}){
     
 
     return (
         <div className='flex justify-center pt-8'>
-            <Youtube videoId="WIRK_pGdIdA" onStateChange={onStateChange}></Youtube>
+            <Youtube videoId="WIRK_pGdIdA" onStateChange={onStateChange} onReady={onReady}></Youtube>
         </div>
     )
 }
